9854233f8acaa2070aa393f75ffc9aaab30a1e6e
Gianni Narracci <gnarracci@gmail.com>
2020-11-02 05:20:35 -0400

    Layers Changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,27 +6,6 @@ export default function Home() {
   return (
     <Card>
       <CardContent>
-        <Typography variant="h4">Crea tu Password Manager</Typography>
-        <Typography variant="h5">Cómo Funciona</Typography>
-        <Typography variant="p">
-          En primer lugar debes crear una contraseña diferente para sus
-          pertenencias electrónicas. No pdrás recuperar tu contraseña, así que
-          recuérdala bien.
-        </Typography>
-        <Typography variant="h5">Qué datos puedes guardar</Typography>
-        <Typography variant="p">
-          Por ejemplo, el número de tu tarjeta, el PIN y el PUK de tu teléfono
-          móvil, el número de seria de uno de tus dispositivos o cualquier
-          información que necesites tener en un lugar seguro.
-        </Typography>
-
-        <Typography variant="h4">Crea tu Password Manager</Typography>
-        <Typography variant="p">
-          En primer lugar, debes crear una contraseña diferente para sus
-          pertenencias electronicas. No podrás recuperar tu contraseña, así que
-          recuerdala bien.
-        </Typography>
-
         <Formik
           validationSchema={Yup.object().shape({
             password: Yup.string()
@@ -56,35 +35,59 @@ export default function Home() {
           onSubmit={() => {}}
         >
           <Form autoComplete="off">
-            <Field
-              name="password"
-              type="password"
-              component={TextField}
-              label="Crea tu Contraseña"
-            />
-            <Field
-              name="repassword"
-              type="password"
-              component={TextField}
-              label="Repita su Contraseña"
-            />
+            <div>
+              <h2>Crea tu Password Manager</h2>
+              <h4>Cómo Funciona</h4>
+              <p>
+                En primer lugar debes crear una contraseña diferente para sus
+                pertenencias electrónicas. No pdrás recuperar tu contraseña, así
+                que recuérdala bien.
+              </p>
+              <h4>Qué datos puede guardar</h4>
+              <p>
+                Por ejemplo, el número de tu tarjeta, el PIN y el PUK de tu
+                teléfono móvil, el número de seria de uno de tus dispositivos o
+                cualquier información que necesites tener en un lugar seguro.
+              </p>
+            </div>
 
-            <Typography variant="p">
-              También puedes crear una pista que te ayude a recordar tu
-              contraseña.
-            </Typography>
-            <Field
-              name="comments"
-              type="text"
-              component={TextField}
-              label="Crea tu pista (opcional)"
-            />
+            <div>
+              <h2>Crea tu Password Manager</h2>
+              <p>
+                En primer lugar, debes crear una contraseña diferente para sus
+                pertenencias electronicas. No podrás recuperar tu contraseña,
+                así que recuerdala bien.
+              </p>
+              <Field
+                name="password"
+                type="password"
+                component={TextField}
+                label="Crea tu Contraseña"
+              />
+              <Field
+                name="repassword"
+                type="password"
+                component={TextField}
+                label="Repita su Contraseña"
+              />
+
+              <Typography variant="p">
+                También puedes crear una pista que te ayude a recordar tu
+                contraseña.
+              </Typography>
+              <Field
+                name="comments"
+                type="text"
+                component={TextField}
+                label="Crea tu pista (opcional)"
+              />
+            </div>
+
+            <div>
+              <h2>Tu Contraseña ha sido creada satisfactoriamente!</h2>
+            </div>
           </Form>
         </Formik>
-
-        <Typography variant="h4">
-          Tu Contraseña a sido Creada Satisfactoriamente!
-        </Typography>
       </CardContent>
     </Card>
   );
